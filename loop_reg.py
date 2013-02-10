24ebae6bee86fa88c92d6309f56b4e6d896d9522
oqadri <oqadri@ualberta.ca>
2013-02-10 09:24:50 -0700

    minor doc update

--- loop_reg.py
+++ loop_reg.py
@@ -2,14 +2,30 @@
 
 This file contains the loop needed in the core_1 function in
 the triple.py file. This is a straightforward implementation,
-focusing on simplicity and correctness. It is probably fast
-enough for interactive use on small images.
+focusing on simplicity and clarity.
 """
 
 from __future__ import division
 import numpy as np
 
 def core(r1,sr,sg,tb,side,lim):
+    """
+    Performs the loop required by the second part of
+    the triple correlation (ie. the core_1 function)
+    
+    Arguments:
+        sr: [2^n 2^n c128] the preprocessed red channel
+        sg: [2^n 2^n c128] the preprocessed green channel
+        tb: [2^n 2^n c128] the complex conjugate of the
+            preprocessed blue channel of the image
+        side: [int] the length of one side of the image
+        lim: [int] the length of one side of the portion of
+            the image over which to perform the computation
+        r1: [lim lim lim lim c128] the array in which
+            to store the results of the computation
+            
+    This function has no return values.
+    """
     na = int(side/2)
     nb = int(lim/2)
     lowlim = na-nb
