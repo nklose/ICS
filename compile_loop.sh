d955250458ecbcf8b4ca5da619ad95b7ec57ac9b
oqadri <oqadri@ualberta.ca>
2013-02-10 11:33:25 -0700

    minor change to docs

--- compile_loop.sh
+++ compile_loop.sh
@@ -1,4 +1,6 @@
 # compiles loop.c using the gcc compiler
+# the only argument is whether or not to
+# enable multithreading (0=disable, 1=enable)
 gcc -O3 -std=c99 -fopenmp -lgomp -fPIC -DMULTI_THREAD=${1} -c loop.c -o loop.o
 gcc -fopenmp -lgomp -shared -Wl,-soname,libloop.so -o libloop.so loop.o
 rm -f loop.o
