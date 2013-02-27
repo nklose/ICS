f9da4832cd35baed1cabf19b15473bf077a4ad2c
oqadri <oqadri@ualberta.ca>
2013-02-27 15:57:09 -0700

    minor changes

--- batch.c
+++ batch.c
@@ -1,6 +1,7 @@
 // python style indenting:
 // :tabSize=4:indentSize=4:noTabs=true:
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <complex.h>
@@ -29,10 +30,10 @@ void core(data,sr,sg,sb,side,lim)
     c128 (*restrict isb)[side] = sb;
     if (lim <= side/2){
         int factor = lim*lim*16;
-        for (int i=0; i<nb; i++)
-            memset(&idata[i][0][0][0],0,(nb-i)*factor);
-        for (int i=nb; i<lim; i++)
-            memset(&idata[i][lim-(i-1)][0][0],0,(i-1)*factor);
+        for (int i=0; i<nb+1; i++)
+            memset(&idata[i][0][0][0],0,((nb+1)-i)*factor);
+        for (int i=nb+1; i<lim; i++)
+            memset(&idata[i][lim-(i-nb)][0][0],0,(i-nb)*factor);
         for (int v1=low; v1<high; v1++){
             int mlow = low-MIN(v1-1,0);
             int mhigh = high-MAX(v1,0);
