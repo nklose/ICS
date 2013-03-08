f73a81529ce465d6e97ffd180bfefa2e301dc9ec
oqadri <oqadri@ualberta.ca>
2013-03-08 08:46:29 -0700

    added separate compile files for 32-bit and 64-bit machines

--- backend/compile_backend_64.sh
+++ backend/compile_backend_64.sh
@@ -1,5 +1,5 @@
-# run with './compile_backend.sh'
+# run with './compile_backend_64.sh'
 gcc -O3 -std=c99 -fPIC -c backend.c -o backend.o
-# change libfftw3_64.a to libfftw3_32.a for 32-bit machines 
+# using libfftw3_64.a for 64-bit machines 
 gcc -shared -Wl,-soname,libbackend.so -o libbackend.so backend.o libfftw3_64.a
 rm -f backend.o
