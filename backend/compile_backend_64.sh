31a12ef546b128207cdb9c012b54a0eef1f592c9
Cameron Mann <camthemann@gmail.com>
2013-03-12 09:38:22 -0600

    Fixed compile scripts

--- backend/compile_backend_64.sh
+++ backend/compile_backend_64.sh
@@ -1,5 +1,5 @@
 # run with './compile_backend_64.sh'
 gcc -O3 -std=c99 -fPIC -c backend.c -o backend.o
 # using libfftw3_64.a for 64-bit machines 
-gcc -shared -Wl,-soname,libbackend.so -o libbackend.so backend.o libfftw3_64.a
+gcc -shared -Wl,-soname,libbackend.so -o libbackend.so backend.o ../vendor/libfftw3_64.a
 rm -f backend.o
