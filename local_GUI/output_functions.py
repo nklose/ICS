c4eb524d07a99c3a84b5712887fcc801b6f21f04
rleung <rleung@ualberta.ca>
2013-03-13 20:07:34 -0600

    Updating output_functions

--- local_GUI/output_functions.py
+++ local_GUI/output_functions.py
@@ -1,7 +1,4 @@
 """
-Code for using matplotlib with Qt4 borrowed from:
-http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
-
 Copryight (c) 2013 Nick Klose, Richard Leung, Cameron Mann, Glen Nelson,
 Omar Qadri, and James Wang under the 401 IP License (see LICENSE file)
 """
@@ -10,27 +7,29 @@ import os.path
 from numpy import arange
 from PyQt4 import QtGui, QtCore
 import matplotlib.pyplot as pp
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
-OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "output")
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 ROOT_DIR = os.path.dirname(CUR_DIR)
 sys.path.append(ROOT_DIR)
+TEMP_DIR = "./ics_tmp"
 
 import backend.dual as dual
 import backend.backend_utils as utils
+import backend.bimloader as bimloader
 
-def auto_cor_gen(self, image, range_val, g0, ginf, color, deltas):
-# stolen from graph.py (depreciated)
-    
-    if not os.path.exists(OUTPUT_DIR):
-        os.mkdir(output_dir)
+# Intended to analyze the image using dual.core and the variables passed in.
+# Then plot the values and axis, saving it to a temporary file.
+# Then load this graph in the temp folder into the specific ui box.
+def auto_cor_gen(self, image, range_val, g0, w, ginf, color, deltas):   
+    if not os.path.exists(TEMP_DIR):
+        os.mkdir(TEMP_DIR)
 
     name_template = "newFG%s.png"
-    fname = os.path.join(directory, name % color)
+    # I hope this is a proper filepath (probably not, TEMP_DIR is wrong)
+    fname = os.path.join(TEMP_DIR, name_template % color)
     # stolen from backend_example.py
     (gnew, gs, used_deltas) = dual.core(image, None, range_val, g0, deltas)
-    # need to figure out w, ginf, and the delta values
+    # need to figure the delta values
     if deltas:
         gfit = utils.gauss_2d(gs, g0, w, ginf)
     else:
@@ -41,6 +40,7 @@ def auto_cor_gen(self, image, range_val, g0, ginf, color, deltas):
             np.arange(1, range_val), gnew[1:, 0], 'o',
             linewidth=2.0)
     pp.axis([0, range_val, 0, max(gfit[0, 0], gnew[1, 0])])
+    # requires a working filepath for me to even test this
     pp.savefig(fname)
     if color == "red":
         self.load_red_output_graph()
@@ -50,7 +50,7 @@ def auto_cor_gen(self, image, range_val, g0, ginf, color, deltas):
         self.load_blue_output_graph()
         
 def load_red_output_graph(self):
-    self.graph = output_dir+fname
+    self.graph = fname
     self.ui.graphFile.setText(basename(self.graph))
     self.ui.imageAutoRed.setPixmap(QtGui.QPixmap(self.graph))
 
@@ -64,3 +64,4 @@ def load_blue_output_graph(self):
     self.ui.graphFile.setText(basename(self.graph))
     self.ui.imageAutoBlue.setPixMap(QtGui.QPixmap(self.graph))
 
+## cross_cor_gen will virtually be a copy paste of auto, once auto is working
